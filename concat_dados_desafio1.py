3215014e3ce31183dc4f4946d66c48c05a7d8cf3
Lucas Gomes <contatolucasgdasilva@gmail.com>
2026-03-05 11:15:36 +0000

    Realizei a solução básica do primeiro desafio.

---
+++ concat_dados_desafio1.py
@@ -0,0 +1,4 @@
+entrada1 = input("Digite o primeiro valor: ")
+entrada2 = input("Digite o segundo valor: ")
+concatenacao = entrada1 + " " + entrada2
+print("A concatenação dos valores é:", concatenacao)
